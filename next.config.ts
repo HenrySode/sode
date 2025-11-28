517ad15a85e4b6f726ecbdde3a715f90ee74e1fe
Henry Maliseli <henrymaliseli027@gmail.com>
2025-11-28 10:51:23 +0300

    debbuging

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    images: {
-        unoptimized: true,
-    },
+
     // baseDir or source directory is automatically src in Next.js 14+; older versions require appDir config
 };
 
