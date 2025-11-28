dce1821ef6a1fa8073133e66636802f536ed9e40
Henry Maliseli <henrymaliseli027@gmail.com>
2025-11-28 10:55:34 +0300

    Disable ESLint during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 const nextConfig = {
 
     // baseDir or source directory is automatically src in Next.js 14+; older versions require appDir config
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = nextConfig;
